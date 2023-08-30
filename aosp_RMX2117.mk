1947afad258c28a526579e2b7f29deba29d09958
Ravinderrohilla <88099034+Ravinderrohilla@users.noreply.github.com>
2023-08-30 13:47:31 +0530

    Add files via upload

---
+++ aosp_RMX2117.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2021 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RMX2117/device.mk)
+
+# Inherit some common Pixel-Experience stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_RMX2117
+PRODUCT_DEVICE := RMX2117
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme narzo 30 pro 5g
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "realme/RMX2117/RMX2117L1:12/SP1A.210812.016/Q.f6e056_1:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2117 \
+    PRODUCT_NAME=RMX2117 \
+    PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1680488352033 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Target
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
